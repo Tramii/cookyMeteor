948dfab4c07be67f65ed970067b2b0d6c460664a
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-03-16 09:19:36 -0500

    react added

---
+++ imports/client/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+export default class App extends Component {
+
+
+	render()
+	{
+		return(
+			<h1>hola</h1>
+		);
+	}
+}
