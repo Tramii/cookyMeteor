eb5204429676554a1b02815accc45081156bb377
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-03-30 02:39:05 -0500

    delete ingredient

--- imports/ui/recipes/addRecipe.jsx
+++ imports/ui/recipes/addRecipe.jsx
@@ -20,48 +20,70 @@ class AddRecipe extends Component{
 
   /*Adds ingredient to ingredient list*/
   addIngredient(event){
-    event.preventDefault();
     this.setState({
-      ingredients : this.state.comentarios.concat(this.state.comentarioActual),
-      comentarioActual:''
+      ingredients : this.state.ingredients.concat(event)
+    }, ()=>{
+       console.log(this.state.ingredients);
     });
   }
 
+  deleteIngredient(ing){
+    console.log('salee');
+    var found = false;
+    var i = 0;
+    while (!found){
+      console.log('busca '+i);
+      if (ing === this.state.ingredients[i]){
+        var temp = this.state.ingredients;
+        found=true;
+        temp.splice(i,1);
+        this.setState({
+          ingredients: temp
+        },()=>{
+          
+        });
+      }
+    }
+  }
+
   /*Sets recipe's instructions*/
-  handleInstructions(event)
-  {
+  handleInstructions(event)  {
     this.setState({
       description: event.target.value
     });
   }
 
   /*Sets recipe's title*/
-  handleTitle(event)
-  {
+  handleTitle(event){
     this.setState({
       title: event.target.value
+    }, ()=>{
+      console.log(this.state.title);
+    });
+  }
+
+  handlePicture(event){
+    this.setState({
+      pictureGif: event.target.value
     })
   }
 
   /*Post recipe to MongoDB*/
-  postRecipe(){
+  saveRecipe(){
     if(this.state.title!=='' && this.state.description!=='' && this.state.ingredients!==[] )
     {
       console.log("esta haciendo el post recipe "+this.state.title+ "  "+this.state.description+"  "+ this.state.ingredients);
-      axios.post(ROOT_URL + "/recipes/addRecipe", {
-        nickName: 'Josega149',
-        password: 'password',
-        folder: 'Favoritos',
-        recipe: {
-                  tipo: 1,
-                  likes: 0,
-                  creadaPor: this.props.username,
-                  title: this.state.title,
-                  description: this.state.description,
-                  pictureGif: this.state.pictureGif,
-                  Ingredients:this.state.ingredients
-                }
-        })
+      var recipe = {
+        "tipo": [1],
+        "likes": 0,
+        "username": Meteor.user().username,
+        "title": this.state.title,
+        "description": this.state.description,
+        "pictureGif": this.state.pictureGif,
+        "Ingredients": this.state.ingredients
+      };
+
+      Meteor.call('recipes.insert', recipe);
     }
     else {
       alert('Some required fields are missing');
@@ -77,20 +99,38 @@ class AddRecipe extends Component{
           <div className="col-md-2"></div>
           <div className ="col-md-8">
             <h1 className="head orange bold">Añadir una nueva receta</h1>
-            <p className="bod">Llena todos los cambios para guardar tu receta</p>
+            <p className="bod">Llena todos los campos para guardar tu receta</p>
 
-            <form className="bod">
+
             <RecipeForm
               title={this.state.title}
               setTitle={this.handleTitle.bind(this)}
+              addIngredient={this.addIngredient.bind(this)}
+              deleteIngredient={this.deleteIngredient.bind(this)}
             />
-            <FormGroup controlId="formControlsFile">
-              <ControlLabel>Adjunta una imagen</ControlLabel>
-              <FormControl type="file"/>
+            <FormGroup className="bod" controlId="formControlsTextarea">
+                <ControlLabel>Instrucciones</ControlLabel>
+                <FormControl
+                  componentClass="textarea"
+                  placeholder="Pasos para hacer tu receta"
+                  value={this.state.description}
+                  onChange={this.handleInstructions.bind(this)}
+                />
             </FormGroup>
-            </form>
 
-            <Button onClick={() => {this.handleTitle.bind(this)}}>Añadir receta</Button>
+            <FormGroup controlId="formControlsText">
+                <ControlLabel>Agrega una imagen o video</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.props.pictureGif}
+                  placeholder="La mejor receta del mundo"
+                  onChange={this.handlePicture.bind(this)}
+                />
+            </FormGroup>
+
+            <Button className="bod" onClick={() => {this.saveRecipe.bind(this)}}>
+              Añadir receta
+            </Button>
           </div>
           <div className="col-md-2"></div>
           </div>
