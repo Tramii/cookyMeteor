57af05daf984c859e0d25a59bf4067979860ae45
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-03-29 23:54:38 -0500

    Error fixed

--- imports/ui/recipes/recipeForm.jsx
+++ imports/ui/recipes/recipeForm.jsx
@@ -1,47 +1,47 @@
-import React, { Component, PropTypes } from 'react';
-import {Button, Well, Table, FormGroup, ControlLabel, FormControl, Checkbox} from 'react-bootstrap';
-import AddedIngredients from './addedIngredients.jsx
-import { createContainer } from 'meteor/react-meteor-data';
-import { UsersWithRecipesCollection } from '../../api/users.js';
+import React, {Component, PropTypes} from 'react';
+import {Button,Well,Table,FormGroup,ControlLabel,FormControl,Checkbox} from 'react-bootstrap';
+import AddedIngredients from './addedIngredients.jsx';
+import {createContainer} from 'meteor/react-meteor-data';
+import {UsersWithRecipesCollection} from '../../api/users.js';
 
-class RecipeForm extends Component{
+class RecipeForm extends Component {
 
+    render() {
+        return (
+            <div>
 
-render(){
-  return (
-    <div>
+                <FormGroup controlId="formControlsText">
+                    <ControlLabel>Título</ControlLabel>
+                    <FormControl type="text" value={this.state.title} placeholder="La mejor receta del mundo" onChange={this.setTitle.bind(this)}/>
+                </FormGroup>
 
-    <FormGroup controlId="formControlsText">
-      <ControlLabel>Título</ControlLabel>
-      <FormControl type="text" value={this.state.title} placeholder="La mejor receta del mundo" onChange={this.setTitle.bind(this)}/>
-    </FormGroup>
+                <FormGroup>
+                    <ControlLabel>Tipo</ControlLabel>
+                    <br/>
+                    <Checkbox inline>
+                        Desayuno
+                    </Checkbox>
+                    {' '}
+                    <Checkbox inline>
+                        Almuerzo o Cena
+                    </Checkbox>
+                    {' '}
+                    <Checkbox inline>
+                        Postre
+                    </Checkbox>
+                </FormGroup>
 
-    <FormGroup>
-      <ControlLabel>Tipo</ControlLabel>
-      <br/>
-      <Checkbox inline>
-        Desayuno
-      </Checkbox>
-      {' '}
-      <Checkbox inline>
-        Almuerzo o Cena
-      </Checkbox>
-      {' '}
-      <Checkbox inline>
-        Postre
-      </Checkbox>
-    </FormGroup>
+                <FormGroup controlId="formControlsText">
+                    <ControlLabel>Ingredientes</ControlLabel>
+                    <AddedIngredients/>
+                </FormGroup>
 
-    <FormGroup controlId="formControlsText">
-      <ControlLabel>Ingredientes</ControlLabel>
-      <AddedIngredients/>
-    </FormGroup>
-
-    <FormGroup controlId="formControlsTextarea">
-      <ControlLabel>Instrucciones</ControlLabel>
-      <FormControl componentClass="textarea" placeholder="Pasos para hacer tu receta" />
-    </FormGroup>
-</div>
-}
+                <FormGroup controlId="formControlsTextarea">
+                    <ControlLabel>Instrucciones</ControlLabel>
+                    <FormControl componentClass="textarea" placeholder="Pasos para hacer tu receta"/>
+                </FormGroup>
+            </div>
+        );
+    }
 }
 export default RecipeForm;
