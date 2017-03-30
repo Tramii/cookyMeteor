9f148a42c712a47b2ae798abd0f6c0c8df883746
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-03-29 23:48:31 -0500

    new component to add ingredients

---
+++ imports/ui/recipes/addedIngredient.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Button, Well} from 'react-bootstrap';
+
+class AddedIngredient extends Component {
+
+  render(){
+    return(
+      <div>
+        <Well>
+          {this.props.text}
+          <Button onClick={()=>this.props.deleteIngredient(this.props.text)}>
+            <i className="fa fa-trash" aria-hidden="true"></i>
+          </Button>
+        </Well>
+      </div>
+    );
+  }
+
+}
+
+export default AddedIngredient;
