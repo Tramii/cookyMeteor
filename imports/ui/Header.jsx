e4c22e35335f6c78b55887f3b27c836d05493bbb
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-03-31 03:40:57 -0500

    finally no errors in lint

--- imports/ui/Header.jsx
+++ imports/ui/Header.jsx
@@ -1,95 +1,92 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import { Template } from 'meteor/templating';
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Accounts from './AccountsUIWrapper.jsx';
-import {createContainer} from 'meteor/react-meteor-data';
+import { createContainer } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 
 class Header extends Component {
-	constructor(props) {
-        super(props);
-
-        this.state = {
-            log: '',
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      log: '',
+    };
   }
 
-	login(x){
-		this.setState({
-			log:x
-		})
-	}
+  login(x) {
+    this.setState({ log: x });
+  }
 
-	render()
-	{
-		var bar;
-		var logueado = Meteor.user();
-		console.log(logueado);
-		if(logueado !== null)
-		{
-			bar = (
-				<Nav>
-						<NavItem className="bod">
-							<Link to="/misRecetas/adicionar">
-								<i className="fa fa-plus" aria-hidden="true"></i> Añadir receta
-							</Link>
-						</NavItem>
-						<NavItem className="bod">
-							<Link to="/misRecetas/ver">
-								<i className="fa fa-cutlery" aria-hidden="true"></i> Mis recetas
-							</Link>
-						</NavItem>
-						<NavItem className="bod">
-							 	<Link to="/general/featured">
-									<i className="fa fa-star" aria-hidden="true"></i> Destacado
-								</Link>
-						</NavItem>
-						<NavItem className="bod">
-								<Link to="/general/search">
-									<i className="fa fa-search" aria-hidden="true"></i> Buscar
-								</Link>
-						</NavItem>
-						<NavItem className="bod">
-								<Link to="/miPerfil">
-									<i className="fa fa-child" aria-hidden="true"></i> Mi perfil
-								</Link>
-						</NavItem>
-						<NavItem className="dropdown pull-center signIn">
-								<Accounts/>
-						</NavItem>
-				</Nav>
-			);
-		}
-		else{
-			bar = (
-				<Nav>
-						<NavItem className="dropdown pull-center signIn"><Accounts/></NavItem>
-				</Nav>
-			);
-		}
-
-		return(
-      <div name = "header">
-				<Navbar collapseOnSelect className="navbar-fixed-top">
-    				<Navbar.Header>
-        				<Navbar.Brand className="head bold orange">
-                				Cooky
-        				</Navbar.Brand>
-								<Navbar.Toggle/>
-    			</Navbar.Header>
-					<Navbar.Collapse>
+  render()
+  {
+    var bar;
+    var logueado = Meteor.user();
+    console.log(logueado);
+    if (logueado !== null) {
+      bar = (
+        <Nav>
+          <NavItem className="bod">
+            <Link to="/misRecetas/adicionar">
+              <i className="fa fa-plus" aria-hidden="true" />
+                    Añadir receta
+            </Link>
+          </NavItem>
+          <NavItem className="bod">
+            <Link to="/misRecetas/ver">
+              <i className="fa fa-cutlery" aria-hidden="true" />
+                   Mis recetas
+            </Link>
+          </NavItem>
+          <NavItem className="bod">
+            <Link to="/general/featured">
+              <i className="fa fa-star" aria-hidden="true" />
+                    Destacado
+            </Link>
+          </NavItem>
+          <NavItem className="bod">
+            <Link to="/general/search">
+              <i className="fa fa-search" aria-hidden="true" />
+                  Buscar
+            </Link>
+          </NavItem>
+          <NavItem className="bod">
+            <Link to="/miPerfil">
+              <i className="fa fa-child" aria-hidden="true" />
+                    Mi perfil
+            </Link>
+          </NavItem>
+          <NavItem className="dropdown pull-center signIn">
+            <Accounts />
+          </NavItem>
+        </Nav>
+            );
+    } else {
+      bar = (
+        <Nav>
+          <NavItem className="dropdown pull-center signIn"><Accounts/></NavItem>
+        </Nav>
+      );
+    }
+    return (
+      <div name="header">
+        <Navbar collapseOnSelect className="navbar-fixed-top">
+          <Navbar.Header>
+            <Navbar.Brand className="head bold orange">
+                Cooky
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
             {bar}
-					</Navbar.Collapse>
-				</Navbar>
-		</div>
-		);
-	}
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
-export default createContainer(() =>{
-	return{
-		currentUser: Meteor.user()
-	};
+export default createContainer(() => {
+    return {currentUser: Meteor.user()};
 }, Header);
--- imports/ui/recipes/recipeForm.jsx
+++ imports/ui/recipes/recipeForm.jsx
@@ -1,47 +1,43 @@
-import React, {Component, PropTypes} from 'react';
-import {Button,Well,Table,FormGroup,ControlLabel,FormControl,Checkbox,HelpBlock} from 'react-bootstrap';
+import React, { Component, PropTypes } from 'react';
+import { FormGroup, ControlLabel, FormControl, Checkbox, HelpBlock } from 'react-bootstrap';
 import AddedIngredients from './addedIngredients.jsx';
-import {createContainer} from 'meteor/react-meteor-data';
-import {UsersWithRecipesCollection} from '../../api/users.js';
+import { createContainer } from 'meteor/react-meteor-data';
+import { UsersWithRecipesCollection } from '../../api/users.js';
 
 class RecipeForm extends Component {
 
-    render() {
-        return (
-            <div className="bod">
-
-                <FormGroup controlId="formControlsText1">
-                    <ControlLabel>Título</ControlLabel>
-                    <FormControl
-                      type="text"
-                      value={this.props.title}
-                      placeholder="La mejor receta del mundo"
-                      onChange={this.props.setTitle}
-                    />
-                </FormGroup>
-
-                <FormGroup>
-                    <ControlLabel>Tipo</ControlLabel>
-                    <br/>
-                    <Checkbox inline onChange={(event)=>{this.props.addType(event.target)}} name="1">Desayuno </Checkbox>
-                    {' '}
-                    <Checkbox inline onChange={(event)=>{this.props.addType(event.target)}} name="2">Almuerzo o Cena </Checkbox>
-                    {' '}
-                    <Checkbox inline onChange={(event)=>{this.props.addType(event.target)}} name="3">Postre </Checkbox>
-                </FormGroup>
-
-                <FormGroup controlId="formControlsText">
-                    <ControlLabel>Ingredientes</ControlLabel>
-                    <AddedIngredients
-                      ingredients={this.props.ingredients}
-                      addIngredient={this.props.addIngredient}
-                      deleteIngredient={this.props.deleteIngredient}
-                    />
-                    <HelpBlock> Presiona Enter para guardar el ingrediente.</HelpBlock>
-                </FormGroup>
-
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="bod">
+        <FormGroup controlId="formControlsText1">
+          <ControlLabel>Título</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.props.title}
+            placeholder="La mejor receta del mundo"
+            onChange={this.props.setTitle}
+          />
+        </FormGroup>
+        <FormGroup>
+          <ControlLabel>Tipo</ControlLabel>
+          <br />
+          <Checkbox inline onChange={(event) => {this.props.addType(event.target); }} name="1">Desayuno </Checkbox>
+          {' '}
+          <Checkbox inline onChange={(event) => {this.props.addType(event.target); }} name="2">Almuerzo o Cena </Checkbox>
+          {' '}
+          <Checkbox inline onChange={(event) => {this.props.addType(event.target); }} name="3">Postre </Checkbox>
+        </FormGroup>
+        <FormGroup controlId="formControlsText">
+          <ControlLabel>Ingredientes</ControlLabel>
+          <AddedIngredients
+            ingredients={this.props.ingredients}
+            addIngredient={this.props.addIngredient}
+            deleteIngredient={this.props.deleteIngredient}
+          />
+          <HelpBlock> Presiona Enter para guardar el ingrediente.</HelpBlock>
+        </FormGroup>
+      </div>
+    );
+  }
 }
 export default RecipeForm;
