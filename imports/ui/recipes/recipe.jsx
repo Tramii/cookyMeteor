cd73208d2da632cff7b2cc8e8d1f05bb16ec4359
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-03-31 03:08:52 -0500

    lint

--- imports/ui/recipes/recipe.jsx
+++ imports/ui/recipes/recipe.jsx
@@ -1,84 +1,83 @@
-import React, {Component, PropTypes} from 'react';
-import {Table, Button, Well} from 'react-bootstrap';
-import Ingredient from './ingredient';
+import React, { Component } from 'react';
+import { Table, Button, Well } from 'react-bootstrap';
+import Ingredient from './ingredient.jsx';
 
 import { createContainer } from 'meteor/react-meteor-data';
 
 class Recipe extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
-    deleteRecipe(){
-      Meteor.call('recipes.remove', this.props.recipe._id);
-    }
+  deleteRecipe() {
+    Meteor.call('recipes.remove', this.props.recipe._id);
+  }
 
-     like(){
-      Meteor.call('recipesLike.update', this.props.recipe._id);
-     }
-    render() {
-        return (
-              <div className="col-md-6 recipe">
-                <Well>
-                  <h3 className="orange head">
-                    {this.props.recipe.title}
-                  </h3>
-                    <Table condensed hover>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <h4 className="bold bod">
-                                      <i className="fa fa-child" aria-hidden="true"></i> Cook
-                                    </h4>
-                                    <p>{this.props.recipe.username}</p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <h4 className="bold bod">
-                                      <i className="fa fa-lemon-o" aria-hidden="true"></i> Ingredients
-                                    </h4>
-                                    <p>
-                                      {this.props.ingredients.map(ingredient =>{
-                                        return(
-                                          <ul key={ingredient.ingrediente}>
-                                              <Ingredient name={ingredient.ingrediente}/>
-                                          </ul>
-                                        );
-                                      })}
-                                    </p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <h4 className="bold bod">
-                                      <i className="fa fa-list-ol" aria-hidden="true"></i> Instructions
-                                    </h4>
-                                    <p>{this.props.recipe.description}</p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <Button className="center" onClick={() => {this.like()}}>
-                                        {this.props.recipe.likes} <i className="fa fa-thumbs-o-up" aria-hidden="true"> </i>
-                                    </Button>
-                                    {' '}
-                                    {this.props.showDelete?
-                                      <Button bsStyle="danger" onClick={() => {this.deleteRecipe()}}>
-                                        <i className="fa fa-trash" aria-hidden="true"> </i> Delete
-                                      </Button>:''}
-                                </td>
-                            </tr>
-                        </tbody>
-                    </Table>
-
-                </Well>
-              </div>
-        );
-    }
+  like() {
+    Meteor.call('recipesLike.update', this.props.recipe._id);
+  }
+  render() {
+    return (
+      <div className="col-md-6 recipe">
+        <Well>
+          <h3 className="orange head">
+            {this.props.recipe.title}
+          </h3>
+          <Table condensed hover>
+            <tbody>
+              <tr>
+                <td>
+                  <h4 className="bold bod">
+                    <i className="fa fa-child" aria-hidden="true" /> Cook
+                  </h4>
+                  <p>{this.props.recipe.username}</p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h4 className="bold bod">
+                    <i className="fa fa-lemon-o" aria-hidden="true" /> Ingredients
+                  </h4>
+                  <p>
+                    {this.props.ingredients.map(ingredient =>{
+                      return (
+                        <ul key={ingredient.ingrediente}>
+                          <Ingredient name={ingredient.ingrediente}/>
+                        </ul>
+                      );
+                    })}
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h4 className="bold bod">
+                    <i className="fa fa-list-ol" aria-hidden="true" /> Instructions
+                  </h4>
+                  <p>{this.props.recipe.description}</p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <Button className="center" onClick={() => {this.like()}}>
+                    {this.props.recipe.likes} <i className="fa fa-thumbs-o-up" aria-hidden="true"> </i>
+                  </Button>
+                  {' '}
+                  {this.props.showDelete?
+                    <Button bsStyle="danger" onClick={() => {this.deleteRecipe()}}>
+                      <i className="fa fa-trash" aria-hidden="true" /> Delete
+                    </Button>:''}
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </Well>
+      </div>
+    );
+  }
 }
 
 export default Recipe;
