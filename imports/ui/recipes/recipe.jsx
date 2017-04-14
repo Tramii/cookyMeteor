aa3a7882733b5cc39b4db8a58ad043f3e6625b92
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-04-13 22:43:26 -0500

    muestra los videos

--- imports/ui/recipes/recipe.jsx
+++ imports/ui/recipes/recipe.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Table, Button, Well } from 'react-bootstrap';
 import Ingredient from './ingredient.jsx';
-
 import { createContainer } from 'meteor/react-meteor-data';
+import HoverBox  from 'react-hoverbox';
 
 class Recipe extends Component {
 
@@ -22,59 +22,89 @@ class Recipe extends Component {
   render() {
     return (
       <div className="col-md-6 recipe">
-        <Well>
-          <h3 className="orange head">
-            {this.props.recipe.title}
-          </h3>
-          <Table condensed hover>
-            <tbody>
-              <tr>
-                <td>
-                  <h4 className="bold bod">
-                    <i className="fa fa-child" aria-hidden="true" /> Cook
-                  </h4>
-                  <p>{this.props.recipe.username}</p>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <h4 className="bold bod">
-                    <i className="fa fa-lemon-o" aria-hidden="true" /> Ingredientes
-                  </h4>
-                  <p>
-                    {this.props.ingredients.map(ingredient =>{
-                      return (
-                        <ul key={ingredient.ingrediente}>
-                          <Ingredient name={ingredient.ingrediente}/>
-                        </ul>
-                      );
-                    })}
-                  </p>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <h4 className="bold bod">
-                    <i className="fa fa-list-ol" aria-hidden="true" /> Instrucciones
-                  </h4>
-                  <p>{this.props.recipe.description}</p>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <Button className="center" onClick={() => {this.like()}}>
-                    {this.props.recipe.likes} <i className="fa fa-thumbs-o-up" aria-hidden="true"> </i>
-                  </Button>
-                  {' '}
-                  {this.props.showDelete?
-                    <Button bsStyle="danger" onClick={() => {this.deleteRecipe()}}>
-                      <i className="fa fa-trash" aria-hidden="true" /> Borrar
-                    </Button>:''}
-                </td>
-              </tr>
-            </tbody>
-          </Table>
-        </Well>
+            <Well>
+              <h3 className="orange head">
+                {this.props.recipe.title}
+              </h3>
+              <HoverBox render = {hover => (
+                  hover?
+                  <Table condensed hover>
+                    <tbody>
+                      <iframe width="420" height="315"
+                      src={recipe.pictureGif}>
+                      </iframe>
+                      <tr>
+                        <td>
+                          <h4 className="bold bod">
+                            <i className="fa fa-list-ol" aria-hidden="true" /> Instrucciones
+                          </h4>
+                          <p>{this.props.recipe.description}</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Button className="center" onClick={() => {this.like()}}>
+                            {this.props.recipe.likes} <i className="fa fa-thumbs-o-up" aria-hidden="true"> </i>
+                          </Button>
+                          {' '}
+                          {this.props.showDelete?
+                            <Button bsStyle="danger" onClick={() => {this.deleteRecipe()}}>
+                              <i className="fa fa-trash" aria-hidden="true" /> Borrar
+                            </Button>:''}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </Table>:
+                  <Table condensed hover>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <h4 className="bold bod">
+                            <i className="fa fa-child" aria-hidden="true" /> Cook
+                          </h4>
+                          <p>{this.props.recipe.username}</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <h4 className="bold bod">
+                            <i className="fa fa-lemon-o" aria-hidden="true" /> Ingredientes
+                          </h4>
+                          <p>
+                            {this.props.ingredients.map(ingredient =>{
+                              return (
+                                <ul key={ingredient.ingrediente}>
+                                  <Ingredient name={ingredient.ingrediente}/>
+                                </ul>
+                              );
+                            })}
+                          </p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <h4 className="bold bod">
+                            <i className="fa fa-list-ol" aria-hidden="true" /> Instrucciones
+                          </h4>
+                          <p>{this.props.recipe.description}</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Button className="center" onClick={() => {this.like()}}>
+                            {this.props.recipe.likes} <i className="fa fa-thumbs-o-up" aria-hidden="true"> </i>
+                          </Button>
+                          {' '}
+                          {this.props.showDelete?
+                            <Button bsStyle="danger" onClick={() => {this.deleteRecipe()}}>
+                              <i className="fa fa-trash" aria-hidden="true" /> Borrar
+                            </Button>:''}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </Table>
+              )}/>
+            </Well>
       </div>
     );
   }
