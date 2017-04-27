9573a953112cf40e7215f2592399f3b6dfc608b6
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-04-26 23:17:43 -0500

    imports

--- imports/ui/Header.test.jsx
+++ imports/ui/Header.test.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Template } from 'meteor/templating';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import Accounts from './AccountsUIWrapper.jsx';
 import Header from './Header.jsx';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import { Factory } from 'meteor/dburles:factory';
 import { shallow, mount } from 'enzyme';
-import { chai, expect } from 'meteor/practicalmeteor:chai';
+import { assert, expect } from 'meteor/practicalmeteor:chai';
 
 
     Meteor.user = function() {
@@ -23,7 +19,7 @@ import { chai, expect } from 'meteor/practicalmeteor:chai';
     describe('Header', function() {
 
         it('Should render correctly', function() {
-          const header = shallow(<Header/>);
-          chai.assert(header.hasClass('header'));
+          const header = shallow(<Header />);
+          assert(header.hasClass('header'));
         })
     })
